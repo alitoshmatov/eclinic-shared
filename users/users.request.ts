879da579d04b7a371528c4944ea199ec74f6e515
Ali Toshmatov <alitoshmatov2001@gmail.com>
2022-09-17 11:26:54 +0500

    Updated user to use departments

--- users/users.request.ts
+++ users/users.request.ts
@@ -1,4 +1,13 @@
-import { IsEnum, IsNotEmpty, IsOptional, IsString, Matches, NotEquals } from "class-validator";
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Matches,
+  NotEquals,
+  ValidateIf,
+} from "class-validator";
 
 export enum Role {
   ADMIN = "ADMIN",
@@ -23,6 +32,12 @@ export class CreateUser {
   @IsEnum(Role)
   @NotEquals(Role.ADMIN)
   role: Role;
+
+  @ValidateIf((item) => item.role === Role.DOCTOR)
+  @IsUUID(undefined, {
+    each: true,
+  })
+  departmentIds: string[];
 }
 
 export class UpdateUser {
@@ -33,14 +48,22 @@ export class UpdateUser {
 
   @IsString()
   @IsNotEmpty()
+  @IsOptional()
   lastName?: string;
 
   @IsString()
   @IsOptional()
+  @Matches(/\+998\d{9}$/)
   phoneNumber?: string;
 
   @IsEnum(Role)
   @NotEquals(Role.ADMIN)
   @IsOptional()
   role?: Role;
+
+  @IsUUID(undefined, {
+    each: true,
+  })
+  @IsOptional()
+  departmentIds?: string[];
 }
