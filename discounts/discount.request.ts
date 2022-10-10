e32658cfa91160812581ac192949c6d321f195d6
Ali Toshmatov <alitoshmatov2001@gmail.com>
2022-10-10 22:36:02 +0500

    Added discount query

--- discounts/discount.request.ts
+++ discounts/discount.request.ts
@@ -1,8 +1,24 @@
-import { IsString, MaxLength, MinLength } from "@alitoshmatov/class-validator";
+import {
+  IsBoolean,
+  IsString,
+  MaxLength,
+  MinLength,
+} from "@alitoshmatov/class-validator";
+import { IsOptional } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class CreateDiscount {
   @IsString()
   @MinLength(3)
   @MaxLength(100)
   name: string;
+}
+
+export class DiscountsQuery {
+  @IsBoolean()
+  @IsOptional()
+  @Transform(({ value }) =>
+    value === "true" ? true : value === "false" ? false : undefined
+  )
+  all?: boolean;
 }
